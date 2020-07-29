33829d669ab7fca86fcb01f9da4266c079365fa5
Nicolas Vega <nicovegag@gmail.com>
2020-07-28 21:59:21 -0400

    Empezando con while

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,49 @@
 function mostrar()
 {
-	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	var estacionIngresada = txtIdEstacion.value;
+	var destinoIngresado = txtIdDestino.value;
+
+
+	switch(estacionIngresada)
+	{
+		case "Invierno":
+			switch(destinoIngresado)
+			{
+				case "Bariloche":
+					alert("Se viaja");
+					break;
+				default:
+					alert("No se viaja");
+			}
+			break;
+		
+		case "Verano":
+			switch(destinoIngresado)
+			{
+				case "Bariloche":
+				case "Cordoba":
+					alert("No se viaja");
+					break;
+				default:
+					alert("Se viaja");
+					break;
+			}
+			break;
+		
+		case "Otoño":
+			default:
+				alert("Se viaja");
+
+		case "Primavera":
+			switch(destinoIngresado)
+			{
+				case "Bariloche":
+					alert("No se viaja");
+					break;
+				default:
+					alert("Se viaja");
+			}
+			break;
+	}
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -3,5 +3,20 @@ al presionar el botón mostrar 10 repeticiones
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert('iteración while');
+	var i; //variable de control
+	
+	i = 0;
+
+	while(i < 10) //Condicion de continuidad del bucle
+	{
+
+		i = i + 1; //contador o i++
+
+		alert(i);
+		//document.write(i);
+		//console.log(i + "<br>");
+	}
+	
+	
+
 }//FIN DE LA FUNCIÓN
